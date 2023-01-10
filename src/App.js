b1d3cbb0240017286de1c7b2a5243012e2e7c974
Thinh Pham <phamvanthinh2001x@gmail.com>
2023-01-10 14:34:23 +0700

    � Clean Architecture

--- src/App.js
+++ src/App.js
@@ -1,128 +1,13 @@
+import AppBar from "components/AppBar/AppBar";
+import BoardBar from "components/BoardBar/BoardBar";
+import BoardContent from "components/BoardContent/BoardContent";
 import "./App.scss";
-
 function App() {
   return (
     <div className="trello">
-      <div className="navbar app">App bar</div>
-      <div className="navbar board">Board Bar</div>
-      <div className="board-columns">
-        <div className="column">
-          <header>Brainstorm</header>
-          <ul>
-            <li>
-              <img
-                className="board-columns__img"
-                src="https://tinyurl.com/2lo45blw"
-                alt="avatar"
-              />
-            </li>
-            <li>Add what you'd like to work on below</li>
-            <li>Add what you'd like to work on below</li>
-            <li>Add what you'd like to work on below</li>
-            <li>Add what you'd like to work on below</li>
-            <li>Add what you'd like to work on below</li>
-            <li>Add what you'd like to work on below</li>
-          </ul>
-          <footer>Add another card</footer>
-        </div>
-        <div className="column">
-          <header>Brainstorm</header>
-          <ul>
-            <li>
-              <img
-                className="board-columns__img"
-                src="https://tinyurl.com/2lo45blw"
-                alt="avatar"
-              />
-            </li>
-            <li>Add what you'd like to work on below</li>
-            <li>Add what you'd like to work on below</li>
-            <li>Add what you'd like to work on below</li>
-            <li>Add what you'd like to work on below</li>
-            <li>Add what you'd like to work on below</li>
-            <li>Add what you'd like to work on below</li>
-          </ul>
-          <footer>Add another card</footer>
-        </div>
-        <div className="column">
-          <header>Brainstorm</header>
-          <ul>
-            <li>
-              <img
-                className="board-columns__img"
-                src="https://tinyurl.com/2lo45blw"
-                alt="avatar"
-              />
-            </li>
-            <li>Add what you'd like to work on below</li>
-            <li>Add what you'd like to work on below</li>
-            <li>Add what you'd like to work on below</li>
-            <li>Add what you'd like to work on below</li>
-            <li>Add what you'd like to work on below</li>
-            <li>Add what you'd like to work on below</li>
-            <li>Add what you'd like to work on below</li>
-            <li>Add what you'd like to work on below</li>
-          </ul>
-          <footer>Add another card</footer>
-        </div>
-        <div className="column">
-          <header>Brainstorm</header>
-          <ul>
-            <li>
-              <img
-                className="board-columns__img"
-                src="https://tinyurl.com/2lo45blw"
-                alt="avatar"
-              />
-            </li>
-            <li>Add what you'd like to work on below</li>
-            <li>Add what you'd like to work on below</li>
-            <li>Add what you'd like to work on below</li>
-            <li>Add what you'd like to work on below</li>
-            <li>Add what you'd like to work on below</li>
-            <li>Add what you'd like to work on below</li>
-          </ul>
-          <footer>Add another card</footer>
-        </div>
-        <div className="column">
-          <header>Brainstorm</header>
-          <ul>
-            <li>
-              <img
-                className="board-columns__img"
-                src="https://tinyurl.com/2lo45blw"
-                alt="avatar"
-              />
-            </li>
-            <li>Add what you'd like to work on below</li>
-            <li>Add what you'd like to work on below</li>
-            <li>Add what you'd like to work on below</li>
-            <li>Add what you'd like to work on below</li>
-            <li>Add what you'd like to work on below</li>
-            <li>Add what you'd like to work on below</li>
-          </ul>
-          <footer>Add another card</footer>
-        </div>
-        <div className="column">
-          <header>Brainstorm</header>
-          <ul>
-            <li>
-              <img
-                className="board-columns__img"
-                src="https://tinyurl.com/2lo45blw"
-                alt="avatar"
-              />
-            </li>
-            <li>Add what you'd like to work on below</li>
-            <li>Add what you'd like to work on below</li>
-            <li>Add what you'd like to work on below</li>
-            <li>Add what you'd like to work on below</li>
-            <li>Add what you'd like to work on below</li>
-            <li>Add what you'd like to work on below</li>
-          </ul>
-          <footer>Add another card</footer>
-        </div>
-      </div>
+      <AppBar />
+      <BoardBar />
+      <BoardContent />
     </div>
   );
 }
---
+++ src/components/BoardBar/BoardBar.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./BoardBar.scss";
+
+const BoardBar = () => {
+  return <div className="navbar-board">Board Bar</div>;
+};
+
+export default BoardBar;
