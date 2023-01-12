f3a27acf517dd8dad27a28cbf64a21f71d61fe8b
Thinh Pham <phamvanthinh2001x@gmail.com>
2023-01-12 11:01:00 +0700

    � Add new column action

--- src/components/BoardContent/BoardContent.js
+++ src/components/BoardContent/BoardContent.js
@@ -1,8 +1,15 @@
 import { initialData } from "actions/initialData";
 import Column from "components/Column/Column";
 import { isEmpty } from "lodash";
-import { useEffect, useState } from "react";
-import { BiPlus } from "react-icons/bi";
+import { useEffect, useRef, useState } from "react";
+import {
+  Button,
+  Col,
+  Container as BootstrapContainer,
+  Form,
+  Row,
+} from "react-bootstrap";
+import { BiPlus, BiX } from "react-icons/bi";
 import { Container, Draggable } from "react-smooth-dnd";
 import { applyDrag } from "utilities/dragDrop";
 import { mapOrder } from "utilities/sorts";
@@ -11,6 +18,10 @@ import "./BoardContent.scss";
 const BoardContent = () => {
   const [board, setBoard] = useState({});
   const [columns, setColumns] = useState([]);
+  const [openNewColumnForm, setOpenNewColumnForm] = useState(false);
+  const [newColumnTitle, setNewColumnTitle] = useState("");
+  const newColumnInputRef = useRef(null);
+
   useEffect(() => {
     const boardFromDB = initialData.boards.find(
       (board) => board.id === "board-1"
@@ -20,6 +31,13 @@ const BoardContent = () => {
       setColumns(mapOrder(boardFromDB.columns, boardFromDB.columnOrder, "id"));
     }
   }, []);
+  useEffect(() => {
+    if (newColumnInputRef && newColumnInputRef.current) {
+      newColumnInputRef.current.focus();
+      newColumnInputRef.current.select();
+    }
+  }, [openNewColumnForm]);
+
   if (isEmpty(board)) {
     return <div className="not-found">Board not found</div>;
   }
@@ -45,6 +63,32 @@ const BoardContent = () => {
     }
   };
 
+  const toggleOpenNewColumnForm = () => {
+    setOpenNewColumnForm(!openNewColumnForm);
+  };
+  const onNewColumnTitleChange = (e) => setNewColumnTitle(e.target.value);
+
+  const addNewColumn = () => {
+    if (!newColumnTitle) {
+      return;
+    }
+    const newColumnToAdd = {
+      id: Math.random().toString(36),
+      boardId: board.id,
+      title: newColumnTitle.trim(),
+      cardOrder: [],
+      cards: [],
+    };
+    console.log(newColumnToAdd);
+    let newColumns = [...columns, newColumnToAdd];
+    let newBoard = { ...board };
+    newBoard.columnOrder = newColumns.map((c) => c.id);
+    newBoard.column = newColumns;
+
+    setColumns(newColumns);
+    setBoard(newBoard);
+    setNewColumnTitle("");
+  };
   return (
     <div className="board-content">
       <Container
@@ -64,10 +108,36 @@ const BoardContent = () => {
           </Draggable>
         ))}
       </Container>
-      <div className="add-new-column">
-        <BiPlus />
-        Add another card
-      </div>
+      <BootstrapContainer>
+        {openNewColumnForm ? (
+          <Row>
+            <Col className="enter-new-column">
+              <Form.Control
+                size="sm"
+                type="text"
+                placeholder="Enter column title..."
+                className="input-enter-new-column"
+                ref={newColumnInputRef}
+                onChange={onNewColumnTitleChange}
+                value={newColumnTitle}
+                onKeyDown={(e) => e.key === "Enter" && addNewColumn()}
+              />
+              <Button variant="success" size="sm" onClick={addNewColumn}>
+                Add Column
+              </Button>
+              <BiX
+                className="cancel-new-column"
+                onClick={() => setOpenNewColumnForm(false)}
+              />
+            </Col>
+          </Row>
+        ) : (
+          <div className="add-new-column" onClick={toggleOpenNewColumnForm}>
+            <BiPlus />
+            Add another card
+          </div>
+        )}
+      </BootstrapContainer>
     </div>
   );
 };
